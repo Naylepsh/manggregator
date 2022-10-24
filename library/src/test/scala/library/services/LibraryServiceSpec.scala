09eb8872ab0c03dfe023c6ee24afff2037ed8748
Naylepsh <naylepsh@gmail.com>
2022-10-24 19:27:34 +0200

    tests: add tests for adding same entity twice

--- library/src/test/scala/library/services/LibraryServiceSpec.scala
+++ library/src/test/scala/library/services/LibraryServiceSpec.scala
@@ -13,6 +13,60 @@ import services.PageRepositoryImpl
 class LibraryServiceSpec extends munit.FunSuite:
   import LibraryServiceSpec._
 
+  // Asset tests
+  test("Cant add the same asset twice") {
+    val assetDto = AssetDTO(
+      name = "Foo",
+      enabled = true,
+      aliases = List(),
+      titlePages = List()
+    )
+
+    val execute = createAsset(assetDto) *> createAsset(assetDto)
+
+    execute.run(storage).map { secondAttempt =>
+      assert(
+        secondAttempt.isLeft,
+        "Second attempt at adding the same asset didnt end in failure"
+      )
+    }
+  }
+
+  // Pages tests
+  test("Cant add the same page twice") {
+    val assetDto = AssetDTO(
+      name = "Foo",
+      enabled = true,
+      aliases = List(),
+      titlePages = List()
+    )
+    val pageDto = AssetPageDTO(site = "bar.baz", url = "http://bar.baz/foo")
+
+    val execute = for {
+      assetE <- createAsset(assetDto)
+      pageInsertionResult <- assetE match {
+        case Right(asset) =>
+          createAssetPage(pageDto, asset.id) *> createAssetPage(
+            pageDto,
+            asset.id
+          )
+        case Left(reason) => Kleisli(_ => IO.pure(reason.asLeft[AssetPage]))
+      }
+    } yield pageInsertionResult
+
+    execute(storage).map { result =>
+      assert(
+        result.isLeft,
+        "Second attempt at adding the same page didnt end in failure"
+      )
+    }
+  }
+
+  // Chapters tests
+  // TODO:
+  // test("Cant add the same chapter twice") {}
+
+  // Misc. tests
   test("Pages added to a given asset appear when getting the assets to crawl") {
     val firstPage = AssetPageDTO(site = "bar.baz", url = "http://bar.baz/foo")
     val secondPage =
