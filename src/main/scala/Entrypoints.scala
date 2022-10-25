eb96a9bf00d1cff64ee9969097b9a8f3243354a4
Naylepsh <naylepsh@gmail.com>
2022-10-25 19:07:11 +0200

    refactor: cleanup Entrypoints

--- src/main/scala/Entrypoints.scala
+++ src/main/scala/Entrypoints.scala
@@ -16,38 +16,6 @@ import library.services.LibraryService.ChapterDTO.apply
 import library.services.LibraryService.ChapterDTO
 
 object Entrypoints:
-  val crawler: Reader[Storage, IO[Unit]] = Reader { storage =>
-    val library = new Library {
-      def getAssetsToCrawl(): IO[List[AssetToCrawl]] =
-        LibraryService
-          .getAssetsToCrawl()
-          .run(storage)
-          .map(_.map { case LibraryService.AssetToCrawl(site, url, title) =>
-            AssetToCrawl(site, title, url)
-          })
-
-      def handleResult(result: Result): IO[Unit] = result match {
-        case ChapterResult(chapters) =>
-          LibraryService
-            .saveChapters(
-              chapters.map(chapter =>
-                ChapterDTO(
-                  chapter.no,
-                  chapter.url,
-                  chapter.dateReleased,
-                  chapter.assetId
-                )
-              )
-            )
-            .run(storage)
-            .void
-      }
-
-    }
-
-    CrawlingService.crawl().run(library)
-  }
-
   def library(storage: Storage): Library = new Library {
     def getAssetsToCrawl(): IO[List[AssetToCrawl]] =
       LibraryService
@@ -59,15 +27,18 @@ object Entrypoints:
 
     def handleResult(result: Result): IO[Unit] = result match {
       case ChapterResult(chapters) =>
-        val data = chapters.map(chapter =>
-          Chapter(
-            randomUUID,
-            chapter.no,
-            chapter.url,
-            chapter.dateReleased,
-            chapter.assetId
+        LibraryService
+          .saveChapters(
+            chapters.map(chapter =>
+              ChapterDTO(
+                chapter.no,
+                chapter.url,
+                chapter.dateReleased,
+                chapter.assetId
+              )
+            )
           )
-        )
-        storage.chapters.save(data)
+          .run(storage)
+          .void
     }
   }
