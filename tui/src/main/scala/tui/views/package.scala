4a7b76444e34fbfb4812be80832699ebf250427c
Naylepsh <naylepsh@gmail.com>
2023-02-04 22:47:53 +0100

    refactor: move context.scala contents to views.package.scala

--- tui/src/main/scala/tui/views/package.scala
+++ tui/src/main/scala/tui/views/package.scala
@@ -6,8 +6,24 @@ import cats.Applicative
 import de.codeshelf.consoleui.elements.PromptableElementIF
 import de.codeshelf.consoleui.prompt._
 import de.codeshelf.consoleui.prompt.builder.ListPromptBuilder
+import library.services.{Assets, Pages}
+import de.codeshelf.consoleui.prompt.ConsolePrompt
+import crawler.domain.Library
+import crawler.services.Crawler
 
 package object views {
+  case class Services[F[_]](
+      assets: Assets[F],
+      pages: Pages[F],
+      crawler: Crawler[F],
+      crawlerLibrary: Library[F]
+  )
+
+  case class Context[F[_]](
+      prompt: ConsolePrompt,
+      services: Services[F]
+  )
+
   private def getListPromptResult(rawPromptResult: PromtResultItemIF): String =
     rawPromptResult.asInstanceOf[ListResult].getSelectedId()
 
